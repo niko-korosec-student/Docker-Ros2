4535ed1483098f96600bfb01b8bd534faf0d9b66
niko-korosec-student <niko.korosec@student.um.si>
2025-09-05 07:51:13 +0000

    interfaces 1.0

--- build/my_py_pkg/colcon_command_prefix_setup_py.sh
+++ build/my_py_pkg/colcon_command_prefix_setup_py.sh
@@ -1,1 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ws/install/my_robot_interfaces/share/my_robot_interfaces/package.sh"
---
+++ build/my_robot_interfaces/my_robot_interfaces__py/CMakeFiles/my_robot_interfaces__py.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/_my_robot_interfaces_s.ep.rosidl_typesupport_c.c"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/_my_robot_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/_my_robot_interfaces_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/__init__.py"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/_hardware_status.py"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/_hardware_status_s.c"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/_led_state_array.py"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/_led_state_array_s.c"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/srv/__init__.py"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/srv/_compute_rectangle_area.py"
+  "/home/ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/srv/_compute_rectangle_area_s.c"
+  "CMakeFiles/my_robot_interfaces__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/my_robot_interfaces__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
